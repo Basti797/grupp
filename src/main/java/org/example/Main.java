fb88a3622711caa6f86c07345e0110e527b9498d
Basti797 <sebastian.spangberg@utb.ecutbildning.se>
2024-10-09 15:43:24 +0200

    ändrade i main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
-        testövning1 test = new testövning1();
+        new testövning1();
     }
 }
